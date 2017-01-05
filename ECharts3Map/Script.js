a834ebf090aca76edb2776426644fea788ce9509
MyronJi <myronji@leapfrogchina.com>
2017-01-05 13:21:05 +0800

    Add:默认值，空值判断

--- ECharts3Map/Script.js
+++ ECharts3Map/Script.js
@@ -1,8 +1,6 @@
 function ECharts3Map_Init() {
     if (document.charset) {
-        //alert(document.charset + "!!!!1");
         document.charset = 'utf-8';
-        //alert(document.charset);
     }
     Qva.AddExtension("ECharts3Map", function () {
         var _this = this;
@@ -21,32 +19,48 @@ function ECharts3Map_Init() {
         jsFiles.push('Extensions/' + _this.ExtSettings.ExtensionName + '/lib/js/jquery.min.js');
         jsFiles.push('Extensions/' + _this.ExtSettings.ExtensionName + '/lib/js/echarts.min.js');
 
-        //读取qv数据
+        //get qv value
         var mapData = [];
         var maxValue = 0;
         var minValue = 50;
-        d = _this.Data
-        for (var i = 0; i < d.Rows.length; i++) {
-            var r = d.Rows[i];
-            obj = {
-                name: r[0].text,
-                value: parseFloat(r[1].text)
-            };
-            if (maxValue < obj.value) { maxValue = obj.value };
-            if (minValue > obj.value) { minValue = obj.value };
-            mapData.push(obj);
+        try {
+            d = _this.Data
+            for (var i = 0; i < d.Rows.length; i++) {
+                var r = d.Rows[i];
+                obj = {
+                    name: r[0].text,
+                    value: parseFloat(r[1].text)
+                };
+                if (maxValue < obj.value) { maxValue = obj.value };
+                if (minValue > obj.value) { minValue = obj.value };
+                mapData.push(obj);
+            }
+
+            var colors = _this.Layout.Text0.text.split(';');
+            var mapFile = _this.Layout.Text1.text.toString();
+
+            //set default value
+            if ('' == maxValue) maxValue = 100;
+            if ('' == minValue) minValue = 0;
+            if ('' == colors) colors = ['lightskyblue', 'yellow', 'orangered'];
+            
+            //Loading up the js files via the QlikView api that allows an array to be passed.   
+            //After we load them up successfully, initialize the chart settings and append the chart
+            Qv.LoadExtensionScripts(jsFiles, function () {
+                InitSettings();
+                Init();
+                if ('' != mapFile) {
+
+                    InitChart(mapFile, mapData, maxValue, minValue, colors);
+                } else {
+                    $(mapchart).html('<div id="errormsg">There was an issue creating the map. Did you forget to set the MapFile?</div> ');
+                }
+
+            });
         }
-
-        var colors = _this.Layout.Text0.text.split(';');
-        var mapFile= _this.Layout.Text1.text.toString();
-
-        //Loading up the js files via the QlikView api that allows an array to be passed.   
-        //After we load them up successfully, initialize the chart settings and append the chart
-        Qv.LoadExtensionScripts(jsFiles, function () {
-            InitSettings();
-            Init();
-            InitChart(mapFile,mapData, maxValue, minValue, colors);
-        });
+        catch (err) {
+            $(mapchart).html('<div id="errormsg">There was an issue creating the map. Did you forget to set the Extensions?</div> ');
+        }
 
         function InitSettings() {
 
@@ -65,16 +79,12 @@ function ECharts3Map_Init() {
             $(_this.Element).append(mapchart);
         }
 
-        function randomData() {
-            return Math.round(Math.random() * 1000);
-        }
-
-        function InitChart(mapFile,mapData, maxValue, minValue, mapColor) {
+        function InitChart(mapFile, mapData, maxValue, minValue, mapColor) {
             try {
                 var myChart = echarts.init(document.getElementById('Chart_' + _this.ExtSettings.UniqueId));
 
                 myChart.showLoading();
-                $.get(_this.ExtSettings.LoadUrl + 'Extensions/ECharts3Map/lib/maps/'+mapFile+'.json').done(function (geoJson) {
+                $.get(_this.ExtSettings.LoadUrl + 'Extensions/ECharts3Map/lib/maps/' + mapFile + '.json').done(function (geoJson) {
                     myChart.hideLoading();
                     echarts.registerMap(mapFile, geoJson);
                     option = {
@@ -114,50 +124,13 @@ function ECharts3Map_Init() {
                                     emphasis: { label: { show: true } }
                                 },
                                 data: mapData
-                                // [
-                                //     { name: '北京', value: randomData() },
-                                //     { name: '天津', value: randomData() },
-                                //     { name: '上海', value: randomData() },
-                                //     { name: '重庆', value: randomData() },
-                                //     { name: '河北', value: randomData() },
-                                //     { name: '河南', value: randomData() },
-                                //     { name: '云南', value: randomData() },
-                                //     { name: '辽宁', value: randomData() },
-                                //     { name: '黑龙江', value: randomData() },
-                                //     { name: '湖南', value: randomData() },
-                                //     { name: '安徽', value: randomData() },
-                                //     { name: '山东', value: randomData() },
-                                //     { name: '新疆', value: randomData() },
-                                //     { name: '江苏', value: randomData() },
-                                //     { name: '浙江', value: randomData() },
-                                //     { name: '江西', value: randomData() },
-                                //     { name: '湖北', value: randomData() },
-                                //     { name: '广西', value: randomData() },
-                                //     { name: '甘肃', value: randomData() },
-                                //     { name: '山西', value: randomData() },
-                                //     { name: '内蒙古', value: randomData() },
-                                //     { name: '陕西', value: randomData() },
-                                //     { name: '吉林', value: randomData() },
-                                //     { name: '福建', value: randomData() },
-                                //     { name: '贵州', value: randomData() },
-                                //     { name: '广东', value: randomData() },
-                                //     { name: '青海', value: randomData() },
-                                //     { name: '西藏', value: randomData() },
-                                //     { name: '四川', value: randomData() },
-                                //     { name: '宁夏', value: randomData() },
-                                //     { name: '海南', value: randomData() },
-                                //     { name: '台湾', value: randomData() },
-                                //     { name: '香港', value: randomData() },
-                                //     { name: '澳门', value: randomData() },
-                                //     { name: '南海诸岛', value: randomData() }
-                                // ]//mapData
                             }
                         ]
                     };
 
                     myChart.setOption(option);
                 });
-                //单击事件
+                //ckick 
                 myChart.on('click', function (params) {
                     _this.Data.SelectRow(params.dataIndex);
                 });
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # ECharts3Map
 - 可以在qlikview的Webview模式中运行的ECharts Map扩展。
+- 支持geoJson格式的地图。
 - 由于时间有限简单做了地图部分的功能，理论上可以使用ECharts的所有图表。
 - 看实际需求，后期有时间在补充。
+
+## 使用方法
+- 请避免同时使用2张地图
+1. Dimension：使用Data.xlsx中的Province/City列。
+2. Expression：结果为数值的表达式。
+3. Color：接收3种写法：#121122;rgba(3,4,5,0.4);red。用英文分号分隔。留空默认为lightskyblue;yellow;orangered
+4. MapFile：使用Data.xlsx中的ProvinceJson，中国为China。
